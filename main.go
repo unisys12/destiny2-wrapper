93c8cbd8b91989ed6edd49043ac23bd6f7f350a2
Phillip Jackson <unisys12@gmail.com>
2021-10-21 06:38:16 -0500

    update working examples.

--- main.go
+++ main.go
@@ -3,22 +3,20 @@ package main
 import (
 	"fmt"
 
-	"github.com/unisys12/destiny2-wrapper/bungie"
+	"github.com/unisys12/destiny2-wrapper/manifest"
 )
 
 func main() {
 	fmt.Println("This is a work in progress")
 
-	manifest, err := bungie.Manifest()
+	version, err := manifest.ManifestVersion()
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
 
-	// Returns the version of the current Manifest
-	// 97762.21.09.15.0038-0-bnet.40222
-	fmt.Println(manifest.Response.Version)
+	fmt.Println(version)
 
 	// Return the MobileGearAssetDataBasePaths Version 0
 	// /common/destiny2_content/sqlite/asset/asset_sql_content_daf3f57e18acbfa94afed6379fb91bd9.content
-	fmt.Printf("%+v", manifest.Response.MobileGearAssetDataBases[0].Path)
+	// fmt.Printf("%+v", manifest.Response.MobileGearAssetDataBases[0].Path)
 }
